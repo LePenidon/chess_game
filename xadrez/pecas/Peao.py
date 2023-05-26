1a0669fb609f1a78a6433948184c8b028e0525ab
lepenidon <penido1000@gmail.com>
2023-05-26 10:45:16 -0300

    peao funciona

--- xadrez/pecas/Peao.py
+++ xadrez/pecas/Peao.py
@@ -15,7 +15,8 @@ class Peao(Peca):
                     (posicao[0], posicao[1] + 1) not in loc_negras and posicao[1] < 7:
                 movimentos.append((posicao[0], posicao[1] + 1))
             if (posicao[0], posicao[1] + 2) not in loc_brancas and \
-                    (posicao[0], posicao[1] + 2) not in loc_negras and posicao[1] == 1:
+                    (posicao[0], posicao[1] + 2) not in loc_negras and (posicao[0], posicao[1] + 1) not in loc_brancas and \
+                    (posicao[0], posicao[1] + 1) not in loc_negras and posicao[1] == 1:
                 movimentos.append((posicao[0], posicao[1] + 2))
             if (posicao[0] + 1, posicao[1] + 1) in loc_negras:
                 movimentos.append((posicao[0] + 1, posicao[1] + 1))
@@ -26,7 +27,8 @@ class Peao(Peca):
                     (posicao[0], posicao[1] - 1) not in loc_negras and posicao[1] > 0:
                 movimentos.append((posicao[0], posicao[1] - 1))
             if (posicao[0], posicao[1] - 2) not in loc_brancas and \
-                    (posicao[0], posicao[1] - 2) not in loc_negras and posicao[1] == 6:
+                    (posicao[0], posicao[1] - 2) not in loc_negras and (posicao[0], posicao[1] - 1) not in loc_brancas and \
+                    (posicao[0], posicao[1] - 1) not in loc_negras and posicao[1] == 6:
                 movimentos.append((posicao[0], posicao[1] - 2))
             if (posicao[0] + 1, posicao[1] - 1) in loc_brancas:
                 movimentos.append((posicao[0] + 1, posicao[1] - 1))

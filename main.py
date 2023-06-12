bc996cb3b79daa15075cf23a286a4998ff47691d
lepenidon <penido1000@gmail.com>
2023-06-12 17:26:01 -0300

    uml

--- main.py
+++ main.py
@@ -2,19 +2,14 @@ import pygame
 from Xadrez import Xadrez
 from Conjuntos import Conjuntos
 
-# cria o objeto jogo
+
 jogo = Xadrez.inicio_jogo()
 
-# cria os conjuntos de peças
-opcoes_negras = jogo.verifica_movimentos('negras')
-opcoes_brancas = jogo.verifica_movimentos('brancas')
-
+opcoes_negras = jogo.conjuntos.verifica_movimentos('negras')
+opcoes_brancas = jogo.conjuntos.verifica_movimentos('brancas')
 rodando = True
 
-# loop principal
 while rodando:
-
-    # atualiza o jogo
     jogo.tempo.tick(jogo.fps)
     if jogo.contador < 30:
         jogo.contador += 1
@@ -22,22 +17,19 @@ while rodando:
         jogo.contador = 0
     jogo.tela.fill('dark grey')
 
-    # mostra o tabuleiro e as peças
-    jogo.mostra_tabuleiro()
-    jogo.mostra_pecas()
-    jogo.mostra_cheque(opcoes_negras, opcoes_brancas)
+    Xadrez.mostra_tabuleiro(jogo)
+    Xadrez.mostra_pecas(jogo)
+    Xadrez.mostra_cheque(jogo, opcoes_negras, opcoes_brancas)
 
-    # mostra os movimentos possíveis
     if jogo.selecao != 100:
-        jogo.movimentos_validos = jogo.verificar_mov_possiveis(
-            opcoes_negras, opcoes_brancas)
-        jogo.mostra_mov_possiveis(jogo.movimentos_validos)
+        jogo.conjuntos.movimentos_validos = Xadrez.verificar_mov_possiveis(
+            opcoes_negras, opcoes_brancas, jogo)
+        Xadrez.mostra_mov_possiveis(jogo.conjuntos.movimentos_validos, jogo)
 
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
 
-        # verifica se o jogador clicou em uma peça
         if evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1 and not jogo.fim:
 
             x_coord = evento.pos[0] // 60
@@ -48,95 +40,86 @@ while rodando:
             jogo.som_fim.stop()
             jogo.som_inicio.stop()
 
-            # realiza o movimento para o turno das brancas
             if jogo.turno <= 1:
 
-                # verifica se o jogador clicou em uma peça
-                if clicou in jogo.brancas.get_posicoes_pecas():
-                    jogo.selecao = jogo.brancas.get_posicoes_pecas().index(clicou)
+                if clicou in jogo.conjuntos.loc_brancas:
+                    jogo.selecao = jogo.conjuntos.loc_brancas.index(clicou)
                     if jogo.turno == 0:
                         jogo.turno = 1
 
-                # verifica se o jogador clicou em um movimento válido
-                if clicou in jogo.movimentos_validos and jogo.selecao != 100:
-                    jogo.brancas[jogo.selecao].posicao = clicou
+                if clicou in jogo.conjuntos.movimentos_validos and jogo.selecao != 100:
+                    jogo.conjuntos.loc_brancas[jogo.selecao] = clicou
                     jogo.som_mov.play()
 
-                    # verifica se o movimento é um xeque-mate
-                    if clicou in jogo.negras.get_posicoes_pecas():
-                        peca_negra = jogo.negras[jogo.negras.index_posicao(
-                            clicou)]
+                    if clicou in jogo.conjuntos.loc_negras:
+                        peca_negra = jogo.conjuntos.loc_negras.index(clicou)
+                        jogo.conjuntos.cap_brancas.append(
+                            jogo.conjuntos.negras[peca_negra])
 
-                        if peca_negra.nome == 'rei':
+                        if jogo.conjuntos.negras[peca_negra] == 'rei':
                             jogo.vencedor = 'brancas'
 
-                        jogo.negras.pecas.pop(
-                            jogo.negras.index_posicao(peca_negra.posicao))
+                        jogo.conjuntos.negras.pop(peca_negra)
+                        jogo.conjuntos.loc_negras.pop(peca_negra)
 
-                    # verifica promoção do peão
-                    jogo.verifica_promocao()
+                    opcoes_negras = jogo.conjuntos.verifica_movimentos(
+                        'negras')
 
-                    # atualiza o jogo
-                    opcoes_negras = jogo.verifica_movimentos('negras')
-                    opcoes_brancas = jogo.verifica_movimentos('brancas')
+                    opcoes_brancas = jogo.conjuntos.verifica_movimentos(
+                        'brancas')
                     jogo.turno = 2
                     jogo.selecao = 100
-                    jogo.movimentos_validos = []
+                    jogo.conjuntos.movimentos_validos = []
 
-            # realiza o movimento para o turno das negras
             if jogo.turno > 1:
-                # verifica se o jogador clicou em uma peça
-                if clicou in jogo.negras.get_posicoes_pecas():
-                    jogo.selecao = jogo.negras.get_posicoes_pecas().index(clicou)
 
+                if clicou in jogo.conjuntos.loc_negras:
+                    jogo.selecao = jogo.conjuntos.loc_negras.index(clicou)
                     if jogo.turno == 2:
                         jogo.turno = 3
-
-                # verifica se o jogador clicou em um movimento válido
-                if clicou in jogo.movimentos_validos and jogo.selecao != 100:
-                    jogo.negras[jogo.selecao].posicao = clicou
+                if clicou in jogo.conjuntos.movimentos_validos and jogo.selecao != 100:
+                    jogo.conjuntos.loc_negras[jogo.selecao] = clicou
                     jogo.som_mov.play()
 
-                    # verifica se o movimento é um xeque-mate
-                    if clicou in jogo.brancas.get_posicoes_pecas():
-                        peca_branca = jogo.brancas[jogo.brancas.index_posicao(
-                            clicou)]
-
-                        if peca_branca.nome == 'rei':
+                    if clicou in jogo.conjuntos.loc_brancas:
+                        white_piece = jogo.conjuntos.loc_brancas.index(clicou)
+                        jogo.conjuntos.cap_negras.append(
+                            jogo.conjuntos.brancas[white_piece])
+                        if jogo.conjuntos.brancas[white_piece] == 'rei':
                             jogo.vencedor = 'negras'
-
-                        jogo.brancas.pecas.pop(
-                            jogo.brancas.index_posicao(peca_branca.posicao))
-
-                    # verifica promoção do peão
-                    jogo.verifica_promocao()
-
-                    # atualiza o jogo
-                    opcoes_negras = jogo.verifica_movimentos('negras')
-                    opcoes_brancas = jogo.verifica_movimentos('brancas')
+                        jogo.conjuntos.brancas.pop(white_piece)
+                        jogo.conjuntos.loc_brancas.pop(white_piece)
+                    opcoes_negras = jogo.conjuntos.verifica_movimentos(
+                        'negras')
+                    opcoes_brancas = jogo.conjuntos.verifica_movimentos(
+                        'brancas')
                     jogo.turno = 0
                     jogo.selecao = 100
-                    jogo.movimentos_validos = []
+                    jogo.conjuntos.movimentos_validos = []
 
-        # verifica se o jogo irá reiniciar
         if evento.type == pygame.KEYDOWN and jogo.fim:
             if evento.key == pygame.K_RETURN:
                 jogo.fim = False
                 jogo.vencedor = ''
-
-                jogo.brancas = Conjuntos("brancas")
-                jogo.negras = Conjuntos("negras")
-
+                jogo.conjuntos.brancas = ['torre', 'cavalo', 'bispo', 'rei', 'rainha', 'bispo', 'cavalo', 'torre',
+                                          'peao', 'peao', 'peao', 'peao', 'peao', 'peao', 'peao', 'peao']
+                jogo.conjuntos.loc_brancas = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
+                                              (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+                jogo.conjuntos.negras = ['torre', 'cavalo', 'bispo', 'rei', 'rainha', 'bispo', 'cavalo', 'torre',
+                                         'peao', 'peao', 'peao', 'peao', 'peao', 'peao', 'peao', 'peao']
+                jogo.conjuntos.loc_negras = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7),
+                                             (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)]
+                jogo.conjuntos.cap_brancas = []
+                jogo.conjuntos.cap_negras = []
                 jogo.turno = 0
                 jogo.selecao = 100
-                jogo.movimentos_validos = []
-                opcoes_negras = jogo.verifica_movimentos('negras')
-                opcoes_brancas = jogo.verifica_movimentos('brancas')
+                jogo.conjuntos.movimentos_validos = []
+                opcoes_negras = jogo.conjuntos.verifica_movimentos('negras')
+                opcoes_brancas = jogo.conjuntos.verifica_movimentos('brancas')
 
-    # verifica se houve um vencedor
     if jogo.vencedor != '':
         jogo.fim = True
-        jogo.mostra_final()
+        Xadrez.mostra_final(jogo)
 
     pygame.display.flip()
 
